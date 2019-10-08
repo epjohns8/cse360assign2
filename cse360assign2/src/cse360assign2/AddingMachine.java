23dce758bb86f1dbdab4272dd79e2185d062b254
johns <epjohns8@asu.edu>
2019-10-08 14:20:29 -0700

    Initial commit of project

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,35 @@
+/* Evan Johnson
+ * #1213632821
+ * CSE 360
+ * Description: this is the adding machine lol
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
