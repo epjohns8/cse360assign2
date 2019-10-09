ca224ab0a0b31c3b4aa7ea9f5dfa4d9b19351766
Evan Johnson <epjohns8@asu.edu>
2019-10-09 16:11:22 -0700

    More comments to make code more clear

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -17,24 +17,29 @@ public class AddingMachine {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	// return the current total as an integer
 	public int getTotal () {
 		return total;
 	}
 	
+	// add the value to the total and add this process to the string
 	public void add (int value) {
 		myString = myString + " + " + value;
 		total = total + value;
 	}
 	
+	// subtract the value from the total and add this process to the string
 	public void subtract (int value) {
 		myString = myString + " - " + value;
 		total = total - value;
 	}
 		
+	// return the current string
 	public String toString () {	
 		return myString;
 	}
 
+	// reset the total and string back to 0;
 	public void clear() {
 		total = 0;
 		myString = "0";
